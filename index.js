d38f813e2f81789145190bd1f9384e883e15cb9f
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-30 22:23:59 +0700

    send data into kafka

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
+
+const produce = require('./producer');
+const consume = require('./consumer');
+
 
 const app = express();
 
@@ -12,13 +16,32 @@ app.get("/", (req, res) => {
 	return res.json({data: "Connected server" });
 });
 
+// try {  
+//   const data = fs.readFileSync('logfile.txt', 'utf8');
+//   console.log(data.toString());    
+// } catch(e) {
+//   console.log('Error:', e.stack);
+// }
+
+
 app.get("/ping", (req, res) => {
   return res.json({data: "pong" })
 });
 
+
 app.get("/health", (req, res) => {
         return res.json({status: `${process.env.APP_NAME} is running on port ${process.env.PORT}`, data: "Application healthy" });
 });
+
+// call the `produce` function and log an error if it occurs
+produce().catch((err) => {
+	console.error("error in producer: ", err)
+})
+
+// start the consumer, and log any errors
+consume().catch((err) => {
+	console.error("error in consumer: ", err)
+})
 
 
 app.listen(process.env.PORT || 3000);
